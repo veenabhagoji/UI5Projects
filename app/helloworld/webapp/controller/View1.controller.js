67f02f6491bf9f4556a20d2e4a562a406a01d250
Veena Bhagoji <veenabhagoji14@gmail.com>
2025-01-09 17:03:34 +0000

    project setup

---
+++ app/helloworld/webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("practice.helloworld.controller.View1", {
+        onInit() {
+        }
+    });
+});
